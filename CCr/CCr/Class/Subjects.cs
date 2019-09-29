9233548ac4d047df929a34acef6a38cc2af11ff1
unknown <enquique500@gmail.com>
2019-09-28 18:00:48 -0600

    Atributos y propiedades

--- CCr/CCr/Class/Subjects.cs
+++ CCr/CCr/Class/Subjects.cs
@@ -8,5 +8,27 @@ namespace CCr.Class
 {
     class Subjects
     {
+        private string description;
+        private double price;
+
+        public string Description { get => description; set => description = value; }
+        public double Price { get => price; set => price = value; }
+
+        public bool create()
+        {
+            return true;
+        }
+        public bool read()
+        {
+            return true;
+        }
+        public bool update()
+        {
+            return true;
+        }
+        public bool delete()
+        {
+            return true;
+        }
     }
 }
